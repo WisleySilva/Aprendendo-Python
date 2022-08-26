f127565dfd67717e8cd364dc87274be4a4840f6d
Wisley Silva <wisley67@hotmail.comgit config --global user.email wisley67@hotmail.com>
2022-08-25 23:10:31 -0300

    Aprendendo sobre laço de repetição

---
+++ aprendendo02.py
@@ -0,0 +1,23 @@
+#criar um contador até 500, ver quais numeros sao divisiveis por 3 e somar todos eles
+
+soma = 0
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont = cont + 1
+        soma = soma + c
+print(f"a soma de todos os numeros é {soma}")
+print(cont)
+
+print("*"*50)
+print("*"*50)
+
+#Criando uma tabuada
+
+num =  int(input("Digite um numero para ver a tabuada: "))
+for x in range(1, 11):
+    print(f"{num} X {x} = {num*x}")
+
+
+print("*"*50)
+print("*"*50)
